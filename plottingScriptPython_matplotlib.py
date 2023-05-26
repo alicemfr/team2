894f4948b961b83102dec378fd14505e794c3895
nicholas clifton <nicholasclifton@MacBook-Pro-10.local>
2023-05-26 15:17:05 +0100

    adjust xlim

--- plottingScriptPython_matplotlib.py
+++ plottingScriptPython_matplotlib.py
@@ -20,6 +20,7 @@ def age_scatter(df, plot_sex, non_linear):
         sns.lmplot(data = df, x = 'Age', y='Beta_percent', scatter = True, lowess = non_linear, scatter_kws={'color': 'black'}, line_kws={'color': 'black'})
         
     plt.ylim(0, 100)
+    plt.xlim(5, 25)
     plt.xlabel("Age (PCW)", fontsize = 15)
     plt.ylabel("DNA methylation (%)", fontsize = 15)
 
