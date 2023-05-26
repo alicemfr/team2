aded9f5e33ee14867288523ef96da1ed727563c3
nicholas clifton <nicholasclifton@MacBook-Pro-10.local>
2023-05-26 15:14:43 +0100

    matplotlib version init

---
+++ plottingScriptPython_matplotlib.py
@@ -0,0 +1,30 @@
+
+from plotnine import *
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import pandas as pd
+# df = pd.read_csv('~/OneDrive - University of Exeter/Documents/Projects/Hackathon_2023/probe1_test.csv')
+
+
+def age_scatter(df, plot_sex, non_linear):
+    
+    # extract info
+    df['Beta_percent'] = df['Value'] * 100  # corresponding DNAm values for probe. Multiplied by 100 for %
+    
+    # plot
+    plt.figure()
+    if plot_sex:
+        sns.lmplot(data = df, x = 'Age', y='Beta_percent', hue = 'Sex', scatter = True, lowess = non_linear)
+    else:
+        sns.lmplot(data = df, x = 'Age', y='Beta_percent', scatter = True, lowess = non_linear, scatter_kws={'color': 'black'}, line_kws={'color': 'black'})
+        
+    plt.ylim(0, 100)
+    plt.xlabel("Age (PCW)", fontsize = 15)
+    plt.ylabel("DNA methylation (%)", fontsize = 15)
+
+    plt.show()
+    
+    
+    
+        
