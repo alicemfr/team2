00a75e78e048a57ff306db269cbec9f85a2cd57d
U-ISAD\jms260 <jms260@5CG1075TGC.isad.isadroot.ex.ac.uk>
2023-05-26 10:10:45 +0100

    add flask site

---
+++ searchr/db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+
+DATABASE = 'site.db'
+		
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
