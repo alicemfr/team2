3a713eee19661aa9cad5319b05e2081448b9acd2
Jessica Shields <jess.m.shields@live.co.uk>
2023-06-09 13:07:47 +0100

    test commit updated

--- searchr/__init__.py
+++ searchr/__init__.py
@@ -5,13 +5,13 @@ from flask import Flask, render_template, request, redirect, url_for, flash, Res
 import sqlite3
 import pandas as pd
 
-import plotting
+from searchr.plotting import test_plot, sex_plot
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "dev"
 
-DATABASE = "searchr/Team2_demo.db"
+DATABASE = "searchr/Team2_Siyi.db"
 
 
 def get_db():
@@ -40,10 +40,16 @@ def pipeline():
     return render_template("pipeline.html", title="Analysis and Pipeline")
 
 
+@app.route("/team")
+def team():
+    return render_template("team.html", title="Meet the plot-Me team")
+
+
 @app.route("/plot", methods=["GET", "POST"])
 def plot():
     if request.method == "POST":
         searchr = request.form["searchr"]
+        # sex = request.form["sex"]
         return redirect(url_for("get_data", gene_name=searchr))
     return render_template("plot.html")
 
@@ -54,32 +60,103 @@ def get_data(
 ):  # this route has been updated to use a template containing a form
     searchr = gene_name
     cursor = get_db().cursor()
-    # search by gene
-    cursor.execute(
-        "SELECT betas.CpG, pheno.Age, betas.Value, pheno.Sex \
-                            from betas, pheno \
-                            WHERE betas.CpG LIKE ?",
-        [searchr],
-    )
-
-    get_db().commit()
-    data = cursor.fetchall()
+    if searchr[0:2] == "cg" and len(searchr) > 9:
+        check = "This is actiavted"
+        cursor.execute(
+            "SELECT PROBEINFO.ProbeKey \
+                FROM PROBEINFO \
+                WHERE PROBEINFO.ProbeName = ?;",
+            [searchr],
+        )
+        result = cursor.fetchall()
+        check = result
+        index_CpG = result[0][0]
+        check1 = index_CpG
+        # Based on the Probe Key, run the corresponding query
+        if index_CpG <= 107899:
+            # check = "Go to AAA table"
+            cursor.execute(
+                "SELECT PROBEINFO.ProbeName, PHENO.Age, PHENO.Sex, AAAVALUE.Value \
+                FROM PROBEINFO, PHENO, AAAVALUE \
+                WHERE AAAVALUE.ProbeKey = ? \
+                AND PROBEINFO.ProbeKey = ? \
+                AND AAAVALUE.SampleKey = PHENO.SampleKey;",
+                [index_CpG, index_CpG],
+            )
+        elif 307899 >= index_CpG > 107899:
+            check = "Go to BBB table"
+            cursor.execute(
+                "SELECT PROBEINFO.ProbeName, PHENO.Age, PHENO.Sex, BBBVALUE.Value \
+                FROM PROBEINFO, PHENO, BBBVALUE \
+                WHERE BBBVALUE.ProbeKey = ? \
+                AND PROBEINFO.ProbeKey = ? \
+                AND BBBVALUE.SampleKey = PHENO.SampleKey;",
+                [index_CpG, index_CpG],
+            )
+        elif 507899 >= index_CpG > 307899:
+            check = "Go to CCC table"
+            cursor.execute(
+                "SELECT PROBEINFO.ProbeName, PHENO.Age, PHENO.Sex, CCCVALUE.Value \
+                FROM PROBEINFO, PHENO, CCCVALUE \
+                WHERE CCCVALUE.ProbeKey = ? \
+                AND PROBEINFO.ProbeKey = ? \
+                AND CCCVALUE.SampleKey = PHENO.SampleKey;",
+                [index_CpG, index_CpG],
+            )
+        elif 807899 >= index_CpG > 507899:
+            check = "Go to DDD table"
+            cursor.execute(
+                "SELECT PROBEINFO.ProbeName, PHENO.Age, PHENO.Sex, DDDVALUE.Value \
+                FROM PROBEINFO, PHENO, DDDVALUE \
+                WHERE DDDVALUE.ProbeKey = ? \
+                AND PROBEINFO.ProbeKey = ? \
+                AND DDDVALUE.SampleKey = PHENO.SampleKey;",
+                [index_CpG, index_CpG],
+            )
+        get_db().commit()
+        data = cursor.fetchall()
+    else:
+        # search by gene
+        cursor.execute(
+            "SELECT betas.CpG \
+                from betas, epic \
+                WHERE epic.CpG = betas.CpG AND\
+                epic.GeneName LIKE ? \
+                LIMIT 1;",
+            [searchr],
+        )
+        get_db().commit()
+        cpg = cursor.fetchall()
+        cursor.execute(
+            "SELECT betas.CpG, pheno.Age, betas.Value, pheno.Sex \
+                                from betas, pheno \
+                                WHERE betas.SampleName = pheno.SampleName AND \
+                                betas.CpG LIKE ?",
+            [cpg],
+        )
+    # get_db().commit()
+    # data = cursor.fetchall()
     CpG_list = []
     Age_list = []
     Value_list = []
     Sex_list = []
     for i in data:
-        CpG, Age, Value, Sex = i
+        CpG, Age, Sex, Value = i
         CpG_list.append(CpG)
         Age_list.append(Age)
-        Value_list.append(Value)
         Sex_list.append(Sex)
-    result_df = pd.DataFrame(list(zip(CpG_list, Age_list, Value_list, Sex_list)))
-    result_df.columns = ["CpG", "Age", "Value", "Sex"]
+        Value_list.append(Value)
+
+    result_df = pd.DataFrame(list(zip(CpG_list, Age_list, Sex_list, Value_list)))
+    result_df.columns = ["CpG", "Age", "Sex", "Value"]
     data = result_df
 
-    plotting.age_scatter()
-    return render_template("table.html", data=data)
+    # plot = age_scatter_save(data, False, False)
+    # plot = test_plot(data)
+    plot = test_plot(data)
+
+    sexplot = sex_plot(data)
+    return render_template("table.html", plot=plot, sexplot=sexplot)
 
 
 if __name__ == "__main__":
