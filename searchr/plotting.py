911e8326a7f9dd33ac34fb9d59195014a144dd2c
Franklin <af498@login02.cluster.local>
2023-06-09 18:23:54 +0100

    added reg line

--- searchr/plotting.py
+++ searchr/plotting.py
@@ -6,6 +6,8 @@ import base64
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import numpy as np
 from matplotlib.figure import Figure
+import statsmodels
+from statsmodels.nonparametric.smoothers_lowess import lowess as use_lowess
 
 # import pandas as pd
 # df = pd.read_csv('~/OneDrive - University of Exeter/Documents/Projects/Hackathon_2023/probe1_test.csv')
@@ -53,7 +55,77 @@ def test_plot(df):
 
     fig, ax = plt.subplots(figsize=(10, 6))
     ax = sns.set_style(style="darkgrid")
-    plt.scatter(data=df, x="Age", y="Beta_percent", color="orange")
+    sns.regplot(data=df, x="Age", y="Beta_percent")
+    #sns.regplot(data=df, x="Age", y="Beta_percent", lowess=True) # for nonlinear fit
+    
+    # attempting to add confidence interval shading to lowess fit...
+    # ...because adding ci=95 to sns.regplot() does not work (https://github.com/mwaskom/seaborn/issues/552)
+    # However, looks like it was recently fixed https://github.com/zeelsheladiya/seaborn/commit/f72896831af24da2dc52961653368ba7438c1c2f
+    # ...but it would seem this change is not present in the version of seaborn.regplot we are using
+    # ...so perhaps we should update seaborn to the newest version and then try...
+    # sns.regplot(data=df, x="Age", y="Beta_percent", lowess=True, ci=95)
+    
+    #lowess_fit = use_lowess(df["Beta_percent"], df["Age"])
+    #lowess_y = lowess_fit[:, 1]
+    #ci = 95
+    #lowess_ci = np.percentile(lowess_fit[:, 1:], [100 - ci / 2, ci / 2], axis=1)
+    #x = df["Age"]
+    #xseq = np.linspace(min(x), max(x), num=91)
+    #plt.plot(xseq, lowess_y)
+    #plt.fill_between(xseq, lowess_ci[0], lowess_ci[1], alpha=0.2)
+    
+    plt.xlabel("Age (PCW)", fontsize=15)
+    plt.ylabel("DNA methylation (%)", fontsize=15)
+    FigureCanvas(fig)
+    img = io.BytesIO()
+    fig.savefig(img, format="png")
+    img.seek(0)
+    figdata_png = base64.b64encode(img.getbuffer()).decode("ascii")
+    return figdata_png
+
+
+
+def sex_plot(df):
+    """TEST"""
+    df["Beta_percent"] = (
+        df["Value"] * 100
+    )  # corresponding DNAm values for probe. Multiplied by 100 for %
+    sns.set_style("white") # white background to match test_plot()
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.scatterplot(x=df["Age"], y=df["Beta_percent"], hue=df["Sex"], s=55) # s = size of points. Increased to match point sizes in test_plot()
+    plt.xlabel("Age (PCW)", fontsize=15)
+    plt.ylabel("DNA methylation (%)", fontsize=15)
+    FigureCanvas(fig)
+    img = io.BytesIO()
+    fig.savefig(img, format="png")
+    img.seek(0)
+    figdata_png = base64.b64encode(img.getbuffer()).decode("ascii")
+    return figdata_png
+    
+    
+    
+    
+    
+## prev versions of plotting functions ##
+    
+def test_plot1(df):
+    """TEST"""
+    df["Beta_percent"] = (
+        df["Value"] * 100
+    )  # corresponding DNAm values for probe. Multiplied by 100 for %
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax = sns.set_style(style="darkgrid")
+    x = df["Age"]
+    y = df["Beta_percent"]
+    #plt.scatter(data=df, x="Age", y="Beta_percent", color="orange") # plot data points
+    plt.scatter(x, y, color="green") 
+    
+    # adding regression line #
+    b, a = np.polyfit(x, y, deg=1) # polynomial of degree 1 i.e. straight line
+    xseq = np.linspace(min(x), max(x), num=100) # seq of 100 values between smallest to largest age
+    plt.plot(xseq, a + b * xseq, color="k", lw=2.5) # add line to plot
+    
     plt.xlabel("Age (PCW)", fontsize=15)
     plt.ylabel("DNA methylation (%)", fontsize=15)
     FigureCanvas(fig)
@@ -82,7 +154,7 @@ def jess_plot(df):
     return figdata_png
     
 
-def sex_plot(df):
+def sex_plot1(df):
     """TEST"""
     df["Beta_percent"] = (
         df["Value"] * 100
