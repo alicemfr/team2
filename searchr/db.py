4ee0793dfbd0d16958c389d60b3be7fcc6bfe41c
Jessica Shields <jess.m.shields@live.co.uk>
2023-08-01 16:46:53 +0100

    toggle plot functionality

--- searchr/db.py
+++ searchr/db.py
@@ -1,18 +1,19 @@
 import sqlite3
 
-import click
 from flask import current_app, g
 
-DATABASE = 'site.db'
-		
+DATABASE = "site.db"
+
+
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is not None:
         db.close()
